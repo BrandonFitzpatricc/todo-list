c6477f0a379a0c7df11c0d582a718d8b67902632
Brandon Fitzpatrick <113076498+BrandonFitzpatricc@users.noreply.github.com>
2026-03-10 12:08:03 -0400

    Fix up CSS

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,4 @@
 import "../stylesheets/custom-reset.css";
 import "../stylesheets/style.css";
 
-document.querySelector("#add-task-dialog").showModal();
+// document.querySelector("#add-task-dialog").showModal();
