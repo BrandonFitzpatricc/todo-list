1dd727ae17325428b2cf566ffa3b17824a29fc00
Brandon Fitzpatrick <113076498+BrandonFitzpatricc@users.noreply.github.com>
2026-03-11 11:28:32 -0400

    Write task module

---
+++ src/scripts/task.js
@@ -0,0 +1,54 @@
+class Task {
+    #id;
+    #title;
+    #description;
+    #dueDate;
+    #priority;
+    #isComplete;
+
+    constructor(title, description, dueDate, priority) {
+        this.#id = crypto.randomUUID();
+        this.#title = title;
+        this.#description = description;
+        this.#dueDate = dueDate;
+        this.#priority = priority;
+        this.#isComplete = false;
+    }
+
+    get id() {
+        return this.#id;
+    }
+
+    get title() {
+        return this.#title;
+    }
+
+    get description() {
+        return this.#description;
+    }
+
+    get dueDate() {
+        return this.#dueDate;
+    }
+
+    get priority() {
+        return this.#priority;
+    }
+
+    get isComplete() {
+        return this.isComplete;
+    }
+
+    edit(title, description, dueDate, priority) {
+        this.#title = title;
+        this.#description = description;
+        this.#dueDate = dueDate;
+        this.#priority = priority;
+    }
+
+    toggleCompletion() {
+        this.#isComplete = !this.#isComplete;
+    }
+}
+
+export { Task };
