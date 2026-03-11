d9eb96446ef69c6f14367ad72fb0a534872df2d4
Brandon Fitzpatrick <113076498+BrandonFitzpatricc@users.noreply.github.com>
2026-03-11 11:46:59 -0400

    Write project manager module

---
+++ src/scripts/project-manager.js
@@ -0,0 +1,15 @@
+import { Project } from "./project.js";
+
+const projects = [];
+
+const addProject = (name) => projects.push(new Project(name));
+
+const deleteProject = (id) => {
+    projects.splice(projects.findIndex(project => project.id === id), 1);
+}
+
+const findProject = (id) => {
+    return projects.find(project => project.id === id);
+}
+
+export { addProject, deleteProject, findProject };
