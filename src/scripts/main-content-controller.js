93b5fb324c75ba57210899ae7daf7ec56ca8f287
Brandon Fitzpatrick <113076498+BrandonFitzpatricc@users.noreply.github.com>
2026-03-26 13:17:59 -0400

    Add functionality to the edit project button

--- src/scripts/main-content-controller.js
+++ src/scripts/main-content-controller.js
@@ -1,5 +1,6 @@
 import { createProjectDisplay } from "./element-factory.js";
-import { getOpenProjects } from "./project-manager.js";
+import { displayProjectTabs } from "./sidebar-controller.js";
+import { findProject, getOpenProjects } from "./project-manager.js";
 import { openNewTaskForm } from "./form-handler.js";
 
 const mainContent = document.querySelector("#main-content");
@@ -13,10 +14,36 @@ const displayOpenProjects = () => {
 
 mainContent.addEventListener("click", (event) => {
     const selectedButton = event.target;
+    // The project display corresponding to the button that was selected is stored to
+    // indicate which project is being modified, which will be accessed through its ID.
+    const currentProjectDisplay = selectedButton.parentNode.parentNode;
 
     if(selectedButton.className === "add-task-btn") {
-        openNewTaskForm(selectedButton.parentNode.parentNode);
+        openNewTaskForm(currentProjectDisplay);
+    }
+
+    if(selectedButton.className === "edit-project-btn") {
+        editProjectName(currentProjectDisplay);
     }
 });
+
+function editProjectName(projectDisplay) {
+    const projectNameInput = projectDisplay.querySelector(".project-name");
+    projectNameInput.readOnly = false;
+
+    const projectNameLength = projectNameInput.value.length;
+    projectNameInput.setSelectionRange(projectNameLength, projectNameLength);
+
+    projectNameInput.focus();
+    ["blur", "keydown"].forEach(eventType => {
+        projectNameInput.addEventListener(eventType, (event) => {
+            if(event.type === "blur" || event.key === "Enter") {
+                findProject(projectDisplay.dataset.id).name = projectNameInput.value;
+                projectNameInput.readOnly = true;
+                displayProjectTabs();
+            }
+        });
+    });
+}
 
 export { displayOpenProjects };
