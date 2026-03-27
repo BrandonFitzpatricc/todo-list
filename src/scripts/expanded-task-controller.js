a2174b9a8c24f2234dc7539552345b4b8aa52721
Brandon Fitzpatrick <113076498+BrandonFitzpatricc@users.noreply.github.com>
2026-03-27 13:50:01 -0400

    Add functionality to the back button

--- src/scripts/expanded-task-controller.js
+++ src/scripts/expanded-task-controller.js
@@ -1,4 +1,5 @@
 import { createExpandedTaskDisplay } from "./element-factory";
+import { displayOpenProjects } from "./project-display-controller";
 
 let currentTask;
 let currentTaskDisplay;
@@ -11,6 +12,22 @@ const displayExpandedTask = (task) => {
     const mainContent = document.querySelector("#main-content");
     mainContent.textContent = "";
     mainContent.appendChild(currentTaskDisplay);
+}
+
+// This event listener can't be attached immediately, because currentTaskDisplay
+// is undefined until an expanded task is displayed
+function attachEventListener() {
+    currentTaskDisplay.addEventListener("click", (event) => {
+        const selectedButton = event.target;
+
+        const buttonHandler = {
+            "back-btn": () => {
+                displayOpenProjects();
+            }
+        }
+
+        buttonHandler[selectedButton.className]();
+    });
 }
 
 export { displayExpandedTask }
